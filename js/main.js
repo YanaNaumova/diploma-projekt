d7b98488b816df77c499c643c0f7f6b98759492c
YanaNaumova <naumovayana87@gmail.com>
2024-07-25 22:26:33 +0200

    image rename, css import

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-// 4. сделать свой селект
 // 5. Js фильтрация
 // 6. Презентация
-// 7. Иконки вынести отдельно
-// 8. Картинки переименовать
-// 9. сursorPointer при наведении
-// 10. проверить название классов на второй странице
-// 11. разбить css
 document.addEventListener("DOMContentLoaded", () => {
   const joinMeetupButton = document.querySelector(".joinMeetup");
 
